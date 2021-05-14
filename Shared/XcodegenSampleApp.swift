8df8bc64dd54dddaf6cd4fe11f7df52e9c2956f5
Daniel R <daniel@macbookpro.local>
2021-05-14 11:46:37 -0400

    Initial Commit

---
+++ Shared/XcodegenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodegenSampleApp.swift
+//  Shared
+//
+//  Created by Daniel R on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct XcodegenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
